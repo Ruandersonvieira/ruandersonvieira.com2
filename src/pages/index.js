a0ff1945438fbc80491af5e74a88ef0fa6b050a0
Ruanderson Vieira <ruanderson.vieira@accalia.io>
2020-04-05 21:22:14 -0300

    :bug: deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,11 @@ import HomeProfile from "../components/HomeProfile"
 import HomePortfolio from "../components/HomePortfolio"
 import HomeBlog from "../components/HomeBlog"
 
-import SEO from "../components/Seo"
+// import SEO from "../components/Seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
+    {/* <SEO title="Home" keywords={[`blog`, `gatsby`, `javascript`, `react`]} /> */}
     <HomeProfile />
     <HomePortfolio />
     <HomeBlog />
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,11 @@
 import React from "react"
 
 import Layout from "../components/Layout"
-import SEO from "../components/Seo"
+// import SEO from "../components/Seo"
 
 const ProjectsPage = () => (
   <Layout>
-    <SEO title="Projects" />
+    {/* <SEO title="Projects" /> */}
     <p>Profile</p>
   </Layout>
 )
